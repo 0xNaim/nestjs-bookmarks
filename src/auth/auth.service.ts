630b47ecaa08ce79cca66697d2e3250d50f6355c
Naim Islam <naimislam3545@gmail.com>
2024-11-10 20:27:47 +0600

    feat: prisma configured

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable({})
+export class AuthService {
+  constructor(private prisma:PrismaService){}
+
+  signup() {
+    return { message: 'I have signed up' };
+  }
+  signin() {
+    return { message: 'I have signed in' };
+  }
+}
