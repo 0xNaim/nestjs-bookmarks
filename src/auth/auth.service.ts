80b8a1ef03478a7939e63bfa1ff6456784481275
Naim Islam <naimislam3545@gmail.com>
2024-11-11 01:15:37 +0600

    feat: user singup and signin feature implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  constructor(private prisma:PrismaService){}
+  constructor(private readonly prisma: PrismaService) {}
 
-  signup() {
-    return { message: 'I have signed up' };
+  async signup(dto: AuthDto) {
+    // Generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    try {
+      // Save the new user to database
+      const user = await this.prisma.user.create({
+        data: {
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+
+      // Return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
   }
-  signin() {
-    return { message: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('Incorrect credentials');
+    }
+
+    const passwordMatched = await argon.verify(user.hash, dto.password);
+
+    if (!passwordMatched) {
+      throw new ForbiddenException('Incorrect credentials');
+    }
+
+    delete user.hash;
+    return user;
   }
 }
